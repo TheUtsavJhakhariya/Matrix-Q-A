dcbcf0aaff4c3e3ffecbc13dff9a0790b540ebf9
Utsav Jhakhariya <86795590+TheUtsavJhakhariya@users.noreply.github.com>
2023-11-06 01:12:49 -0500

    Create matrixqa.js

---
+++ matrixqa.js
@@ -0,0 +1,14 @@
+        const button = document.getElementById('runaway-button');
+        const maxX = window.innerWidth - button.clientWidth;
+        const maxY = window.innerHeight - button.clientHeight;
+
+        button.addEventListener('mousemove', () => {
+            const randomX = Math.floor(Math.random() * maxX);
+            const randomY = Math.floor(Math.random() * maxY);
+            button.style.left = randomX + 'px';
+            button.style.top = randomY + 'px';
+        });
+
+        button.addEventListener('click', (e) => {
+            e.stopPropagation();
+        });
